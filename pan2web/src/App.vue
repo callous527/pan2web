b6bb7096f5791cd5fe5c7805f7115d85d3105105
callous527 <callous527@hotmail.com>
2019-03-07 23:21:52 +0800

    將App.vue增加Menu清單並使用vue-router關聯至TodoList

--- pan2web/src/App.vue
+++ pan2web/src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <div class="menu">
+      <router-link to="/TodoList">TodoList</router-link>
+    </div>
+    <div>
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
+import TodoList from "./components/TodoList.vue";
+import VueRouter from "vue-router";
 
-export default {};
+export default {
+  router: new VueRouter({
+    routes: [
+      { path: "/TodoList", component: TodoList },
+    ]
+  }),
+  components: {
+    TodoList
+  }
+};
 </script>
